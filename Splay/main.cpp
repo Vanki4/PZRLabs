e94bff55a13f4b5f414aca9f653ede40a8fe7b73
Иван Полтаранос <vanki4@MacBook-Air-Ivan.local>
2026-06-01 21:04:13 +0300

    Добавлен main с демонстрацией работы

---
+++ Splay/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "splay_tree_api.h"
+#include <string>
+
+int main() {
+	SplayTree<std::string,double> a;
+	a.insert("cat",10.0);
+	a.insert("dog",15.0);
+	a.insert("pig",20.0);
+	a.insert("DaniilMandrikin",100.0);
+	auto i = a.search("dog");
+	if (i)
+	{
+		std::cout << "Найдено: " << *i << std::endl;
+	}
+	else std::cout << "Ошибка" << std::endl;
+	a.remove("DaniilMandrikin");
+	std::cout << "Размер: " << a.size() << std::endl;
+	return 0;	
+}
